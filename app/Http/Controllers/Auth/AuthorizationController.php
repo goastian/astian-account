3340093f384550ff0da13c2a68cc00e94625e135
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-01-07 18:23:00 -0500

    agregando assets, Autorizacion y tokens

---
+++ app/Http/Controllers/Auth/AuthorizationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+ 
+use App\Assets\Device;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Controllers\GlobalController as Controller;
+
+class AuthorizationController extends Controller
+{ 
+    use  Device;
+
+    public function __construct()
+    {
+        $this->middleware('guest')->only('store');
+        $this->middleware('auth:sanctum')->only('destroy'); 
+    }
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(LoginRequest $request)
+    {
+        $request->authenticate();
+
+        $token = request()->user()->createToken($this->setTokenName($request));
+
+        return response()->json(['data' => [ 
+            'Authorization' => "Bearer " . $token->plainTextToken,
+        ]], 201);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy()
+    {
+        request()->user()->currentAccessToken()->delete();
+
+        return response()->json(['data' => [
+            'message' => 'La sesion ha sido cerrada.'
+        ]], 200);
+    }
+}
