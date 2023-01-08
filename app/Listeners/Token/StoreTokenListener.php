498d100d6f5a915ae1084d7f7cd4963c690cfce3
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-01-07 20:41:21 -0500

    agregando eventos

---
+++ app/Listeners/Token/StoreTokenListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners\Token;
+
+use App\Events\Token\StoreTokenEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class StoreTokenListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\Token\StoreTokenEvent  $event
+     * @return void
+     */
+    public function handle(StoreTokenEvent $event)
+    {
+        //
+    }
+}
