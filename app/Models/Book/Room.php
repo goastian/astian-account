78cfd98f2571f33fb5da8632c58646adb5111068
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-04-14 15:04:44 -0500

    agregan recursos para el modelo Room

---
+++ app/Models/Book/Room.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\Book;
+
+use App\Transformers\Asset\RoomTransformer;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Room extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $table = "rooms";
+
+    public $view = "room";
+
+    public $transformer = RoomTransformer::class;
+
+    protected $fillable = [
+        'number',
+        'description',
+        'category_id'
+    ];
+
+    public function setNumberAttribute($value)
+    {
+        $this->attributes['number'] = strtoupper($value);
+    }
+
+    public function setDescriptionAttribute($value)
+    {
+        $this->attributes['description'] = strtolower($value);
+    }    
+
+}
