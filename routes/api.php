d0acc7b7f8cc67bd45ee3a0ae5fe6de36dff856d
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-04-17 08:34:23 +0000

    Revert "Merge branch 'assets' into 'base'"
    This reverts merge request !3

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,20 @@ use App\Http\Controllers\Asset\CategoryCalendarController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Asset\CategoryController;
 use App\Http\Controllers\Asset\RoomController; 
- 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\AuthorizationController;
+use App\Http\Controllers\Sanctum\TokensController;
+
+//Auth
+Route::post('login', [AuthorizationController::class,'store']);
+Route::post('logout', [AuthorizationController::class, 'destroy']);
+Route::get('about', [AuthenticatedSessionController::class, 'profile']);
+
+Route::get('tokens',[TokensController::class, 'index'])->name('tokens.index');
+Route::post('tokens', [TokensController::class, 'store'])->name('tokens.store');
+Route::delete('tokens/clean', [TokensController::class, 'destroyAllTokens'])->name('tokens.clean');
+Route::delete('tokens/{id}', [TokensController::class, 'destroy'])->name('tokens.destroy');
+
 //categories
 Route::delete('categories/{category}/disable', [CategoryController::class, 'disable'])->name('categories.disable');
 Route::get('categories/{id}/enable', [CategoryController::class, 'enable'])->name('categories.enable');
@@ -17,4 +30,6 @@ Route::delete('rooms/{room}/disable', [RoomController::class, 'disable'])->name(
 Route::get('rooms/{id}/enable', [RoomController::class, 'enable'])->name('rooms.enable');
 Route::resource('rooms', RoomController::class)->except('edit', 'create');
 
+//calendar
 Route::resource('calendars', CalendarController::class)->only('index');
+
