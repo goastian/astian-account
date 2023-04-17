d3ef28c2be9f0f5adf425d56df046744a45816ce
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-04-17 02:48:17 -0500

    restructucturando models and controllers

--- routes/auth.php
+++ routes/auth.php
@@ -1,19 +1,4 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController; 
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController; 
-use Illuminate\Support\Facades\Route;
-
  
-
-Route::get('/login', [AuthenticatedSessionController::class, 'create']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
-
-Route::get('/forgot-password', [PasswordResetLinkController::class, 'create']);
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-
-Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.store');
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+ 
