55054242aa3828f4f93ad97ca02790498ae38654
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-04-25 18:52:01 -0500

    agregando recursos de CategoryCalendar

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Asset\CalendarController;
+use App\Http\Controllers\Asset\CategoryCalendarController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Asset\CategoryController;
 use App\Http\Controllers\Asset\RoomController; 
@@ -8,8 +10,11 @@ use App\Http\Controllers\Asset\RoomController;
 Route::delete('categories/{category}/disable', [CategoryController::class, 'disable'])->name('categories.disable');
 Route::get('categories/{id}/enable', [CategoryController::class, 'enable'])->name('categories.enable');
 Route::resource('categories', CategoryController::class)->except('edit','create');
+ROute::resource('categories.calendars', CategoryCalendarController::class)->except('edit', 'create','destroy');
 
 //rooms
 Route::delete('rooms/{room}/disable', [RoomController::class, 'disable'])->name('rooms.disable');
 Route::get('rooms/{id}/enable', [RoomController::class, 'enable'])->name('rooms.enable');
 Route::resource('rooms', RoomController::class)->except('edit', 'create');
+
+Route::resource('calendars', CalendarController::class)->only('index');
