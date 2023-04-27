a19fbc139f96c7aaffef23fc9413c0a00dd76a89
Elvis Yerel Roman Concha <yerel9212@yahoo.es>
2023-04-27 17:51:42 -0500

    actualizando controlador

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -8,15 +8,24 @@ use Illuminate\Http\Request;
 class GlobalController extends Controller
 {
     use JsonResponser;
-    
+
     public function __construct()
     {
         $this->middleware('auth:sanctum');
     }
 
-
     public function AuthKey()
     {
-       return request()->user()->id;
+        return request()->user()->id;
+    }
+
+    public function lowercase($value)
+    {
+        return strtolower($value);
+    }
+
+    public function uppercase($value)
+    {
+        return strtoupper($value);
     }
 }
