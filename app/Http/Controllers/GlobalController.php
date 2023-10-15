25952f9bfed1131401a4fe119b0a0d5cc2579883
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-10-15 12:40:17 -0500

    agregando ability en el constructor

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -12,12 +12,18 @@ use Illuminate\Routing\Controller;
 class GlobalController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, JsonResponser, Asset;
-    
+
     public $can_update = array();
 
     public function __construct()
     {
         $this->middleware('auth:sanctum');
+        $this->middleware('ability:admin,read')->only('index', 'show');
+        $this->middleware('ability:admin,write')->only('store');
+        $this->middleware('ability:admin,update')->only('update');
+        $this->middleware('ability:admin,enable')->only('enable');
+        $this->middleware('ability:admin,disable')->only('disable');
+        $this->middleware('ability:admin,destroy')->only('destroy');
     }
 
     public function AuthKey()
