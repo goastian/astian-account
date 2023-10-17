75ee70d0fed33fe2a84a049993159d61c9898ec1
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-10-17 15:17:09 -0500

    add bue componentes oauth

---
+++ resources/js/Pages/OAuth/Components/table.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="table-responsive mt-3">
+    <table :class="['table table-hover table-dark', styles]">
+      <thead>
+        <th
+          scope="col"
+          class="text-uppercase"
+          v-for="(item, index) in items"
+          :key="index"
+          v-text="item"
+        ></th>
+      </thead>
+      <tbody>
+        <slot name="body"></slot>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    styles: {
+      type: [String, Array],
+      required: false,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    /* title: {
+            type: String,
+            required: false,
+            default: 'Data'
+        }, 
+        icon: {
+            type: String,
+            required: false,
+            default: "fa-circle-arrow-left fa-2x fa-beat pull-right"
+        },*/
+    uri: {
+      type: String,
+      required: false,
+      default: "home",
+    },
+  },
+};
+</script>
+<styles scoped lang="css">
+table {
+  caption-side: top;
+}
+</styles>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @env(['prod', 'production'])
-    <link rel="stylesheet" href="{{ secure_asset('dist/app.css') }}">
-    @endenv
-
-    @env(['local', 'dev'])
-    <link rel="stylesheet" href="{{ asset('dist/app.css') }}">
-    @endenv
+      <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
