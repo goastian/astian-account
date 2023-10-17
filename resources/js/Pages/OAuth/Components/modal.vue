683e9fd212b80be939174192cabb5aa29ba38ef5
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-10-17 15:17:09 -0500

    add bue componentes oauth

---
+++ resources/js/Pages/OAuth/Components/modal.vue
@@ -0,0 +1,62 @@
+<template>
+   <!-- Button trigger modal -->
+<button type="button" :class="['btn', 'btn-primary', styles]" @click="sendEvent1(id)" data-bs-toggle="modal" :data-bs-target="('#').concat(target)">
+   <slot name="button"></slot>
+</button>
+
+<!-- Modal -->
+<div :class="['modal', 'fade', width]" :id="target" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+         <slot name="head"></slot>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+          <slot name="body"></slot>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" @click="sendEvent2(id)">Aceptar</button>
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+
+    emits:['isClicked', 'isAccepted'],
+
+     props:{
+        target:{
+            type:[String, Array],
+            required:true
+        }, 
+        
+        width:{
+            type:String,
+            default: "modal-lg"
+        },
+        styles:{
+          type:[String, Array],
+          default: 'btn-sm'
+        }
+     },
+ 
+     methods: {
+        sendEvent1(id){
+            this.$emit('isClicked', id)
+        },
+
+        sendEvent2(id){
+            this.$emit('isAccepted', id)
+        }
+     },
+
+
+}
+</script>
+<style lang="">
+    
+</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,69 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+        <div class="container-fluid">
+            <a class="navbar-brand fw-bold text-xl text-success" href="{{ route('dashboard')}}">
+                <v-icon styles="fa-home pull-left"></v-icon>
+                Home
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{route('clientes')}}">Clientes</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav text-md">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-lg-end dropdown-menu-dark">
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <form action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <button type="submit"
+                                        class="dropdown-item text-md">
+                                        Cerrar
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    @inertia
+</body>
+
 </html>
