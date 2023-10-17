385a740f3531d80521a504d456e91019984454ad
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-10-17 15:16:24 -0500

    cooment ingnore route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Passport::ignoreRoutes();
-
+        //Passport::ignoreRoutes();
     }
 
     /**
