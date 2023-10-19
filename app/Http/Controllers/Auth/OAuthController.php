9b2bf5179665c8b2b3aca446f88599e0600a9890
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-10-18 19:44:07 -0500

    add function

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class OAuthController extends Controller
@@ -18,7 +20,26 @@ class OAuthController extends Controller
         return Inertia::render('Dashboard');
     }
 
-    public function clientes(){
-        return Inertia::render('OAuth/Clients');
+    public function clientes()
+    {
+        return Inertia::render('OAuth/Clients', [
+            'URL' => env('APP_URL'),
+            'session' => route('session.update'),
+        ]);
+    }
+
+    public function tokens()
+    {
+        return Inertia::render('OAuth/Tokens');
+    }
+
+    public function sessionState(Request $request)
+    {
+        if (!$request->wantsJson()) {
+            return redirect()->route('dashboard');
+        }
+        $request->session()->put('state', $state = Str::random(40));
+
+        return $state;
     }
 }
