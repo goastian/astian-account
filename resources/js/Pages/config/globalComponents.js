dee0c7332ed20efec56cc3cb99fe41a0203bf528
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-10-23 01:17:03 -0500

    add new properties

--- resources/js/Pages/config/globalComponents.js
+++ resources/js/Pages/config/globalComponents.js
@@ -3,6 +3,8 @@ import VTable from "../Components/table.vue";
 import VModal from "../Components/modal.vue";
 import VScopes from "../Components/scopes.vue";
 import VError from "../Components/errors.vue";
+import VNav from "../Dashboad/Navbar.vue";
+import VItem from "../Dashboad/NavItem.vue";
 
 export const components = [
     ["VPagination", VPagination],
@@ -10,4 +12,6 @@ export const components = [
     ["VModal", VModal],
     ["VScopes", VScopes],
     ["VError", VError],
+    ["VNav", VNav],
+    ["VItem", VItem],
 ];
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,19 +4,17 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common["Accept"] = "application/json"; 
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.headers.common["Accept"] = "application/json";
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
-
-// import Echo from 'laravel-echo';
-
+ 
 // import Pusher from 'pusher-js';
 // window.Pusher = Pusher;
 
