d130022aaf46edb2baf82e12c2f20e14af7217a1
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-10-22 20:42:37 -0500

    add global components

---
+++ resources/js/Pages/config/globalComponents.js
@@ -0,0 +1,13 @@
+import VPagination from "../Components/pagination.vue";
+import VTable from "../Components/table.vue";
+import VModal from "../Components/modal.vue";
+import VScopes from "../Components/scopes.vue";
+import VError from "../Components/errors.vue";
+
+export const components = [
+    ["VPagination", VPagination],
+    ["VTable", VTable],
+    ["VModal", VModal],
+    ["VScopes", VScopes],
+    ["VError", VError],
+];
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,36 @@
-import './bootstrap';
-import '../scss/app.scss';
+import "./bootstrap";
+import "../scss/app.scss";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-import {router} from './Pages/config/rutes.js'
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import { router } from "./Pages/config/rutes.js";
+import { components } from "./Pages/config/globalComponents";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
- 
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/Dashboard.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/Dashboard.vue")
+        ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(router)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
+        app = createApp({ render: () => h(App, props) });
+
+        app.use(plugin);
+        app.use(router);
+        app.use(ZiggyVue, Ziggy);
+
+        components.forEach((index) => {
+            app.component(index[0], index[1]);
+        });
+
+        return app.mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
