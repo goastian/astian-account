9bf6b678131baa90f215ec2527350bbafb292be9
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-10-23 13:03:27 -0500

    Configurando eventos

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,14 @@ import App from "./App.vue";
 
 import { router } from "./config/rutes.js";
 import { components } from "./config/globalComponents";
-//import { $channels, $echo} from "./config/laravel-echo"
+import { $channels, $echo } from "./config/laravelEcho";
 
 import "./bootstrap";
 import "../scss/app.scss";
 
-  
-app = createApp(App);
-// app.config.globalProperties.$echo = $echo
-// app.config.globalProperties.$channels = $channels;
+const app = createApp(App);
+app.config.globalProperties.$echo = $echo;
+app.config.globalProperties.$channels = $channels;
 app.use(router);
 
 components.forEach((index) => {
