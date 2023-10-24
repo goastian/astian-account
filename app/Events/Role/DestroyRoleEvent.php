94328c93f512528c3a680e77deee86f76651ddef
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-10-24 10:47:57 -0500

    add funtion to remove scopes

---
+++ app/Events/Role/DestroyRoleEvent.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events\Role;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels; 
+
+
+class DestroyRoleEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user = null;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user = null)
+    { 
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel(env('CHANNEL_NAME','auth'));
+    }
+
+     /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'DestroyRoleEvent';
+    }
+}
---
+++ app/Listeners/Role/DestroyRoleListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners\Role;
+
+use App\Events\Role\DestroyRoleEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class DestroyRoleListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(DestroyRoleEvent $event): void
+    {
+        //
+    }
+}
