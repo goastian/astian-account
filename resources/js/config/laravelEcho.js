586d68c7fa97f29185e9189504bbdf88c00e4fb2
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-11-06 14:21:56 -0500

    agregando variable de entorno

--- resources/js/config/laravelEcho.js
+++ resources/js/config/laravelEcho.js
@@ -1,22 +1,22 @@
 import Echo from "laravel-echo";
-import io from "socket.io-client";   
+import io from "socket.io-client";
 
 window.io = io;
 
 const options = {
-  broadcaster: "socket.io",
-  host: import.meta.env.VITE_APP_LARAVEL_ECHO,
-  transports: ["websocket", "polling", "flashsocket"],  
+    broadcaster: "socket.io",
+    host: import.meta.env.VITE_APP_LARAVEL_ECHO,
+    transports: ["websocket", "polling", "flashsocket"],
 };
 
 export const $echo = new Echo(options);
 
 export const $channels = {
-  ch_0() {
-    return `auth`;
-  },
+    ch_0() {
+        return import.meta.env.VITE_APP_LARAVEL_ECHO_CHANNEL;
+    },
 
-  ch_1(id) {
-    return `auth.${id}`;
-  },
+    ch_1(id) {
+        return import.meta.env.VITE_APP_LARAVEL_ECHO_CHANNEL + `.${id}`;
+    },
 };
