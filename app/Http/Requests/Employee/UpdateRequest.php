8e109cbd8a6a387b1a0bb527446ebf0b8c6c0c69
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2023-12-28 10:01:00 -0500

    Alter migration and Employee model

--- app/Http/Requests/Employee/UpdateRequest.php
+++ app/Http/Requests/Employee/UpdateRequest.php
@@ -29,11 +29,10 @@ class UpdateRequest extends FormRequest
             'name' => ['nullable', 'max:100'],
             'last_name' => ['nullable', 'max:100'],
             'email' => ['nullable', 'email', 'max:100', 'unique:employees,email,' . Request('user')->id],
-            'document_type' => ['nullable', Rule::in(EnumType::documento_type())],
-            'document_number' => ['nullable', 'max:12', 'unique:employees,document_number,' . Request('user')->id],
             'country' => ['nullable', 'max:100'],
-            'department' => ['nullable', 'max:100'],
+            'city' => ['nullable', 'max:100'],
             'address' => ['nullable', 'max:150'],
+            'birthday' => ['nullable', 'date_format:Y-m-d'],
             'phone' => ['nullable', 'max:9', 'unique:employees,phone,' . request()->user->id]
         ];
     }
