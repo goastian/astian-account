befe3d2bdad709eb48a7c21d409dd05f0df1f836
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-12-29 10:50:35 -0500

    adding automatic schedule to destroy clients unverify

--- app/Models/User/Employee.php
+++ app/Models/User/Employee.php
@@ -2,13 +2,14 @@
 
 namespace App\Models\User;
 
-use DateTime;
-use DateInterval;
-use App\Models\Auth;
 use App\Events\Employee\DestroyEmployeeEvent;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Transformers\Auth\EmployeeTransformer;
+use App\Models\Auth;
 use App\Notifications\Client\DestroyClientNotification;
+use App\Transformers\Auth\EmployeeTransformer;
+use DateInterval;
+use DateTime;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Employee extends Auth
 {
@@ -37,13 +38,34 @@ class Employee extends Auth
         $users = Employee::onlyTrashed()->where('client', 1)->where('deleted_at', "<", $fecha)->get();
 
         if (count($users) > 0) {
-            
+
             foreach ($users as $user) {
                 $user->notify(new DestroyClientNotification());
                 DestroyEmployeeEvent::dispatch();
                 $user->forceDelete();
             }
 
+        }
+    }
+
+    /**
+     * remove unverified accounts 
+     */
+    public static function remove_clients_unverified()
+    {
+        $now = new DateTime();
+        $now->sub(new DateInterval('PT' . env('TIME_TO_VERIFY_ACCOUNT', 5) . 'M'));
+        $fecha = $now->format('Y-m-d H:i:s');
+
+        $deleted = DB::table('employees')
+            ->where('verified_at', null)
+            ->where('created_at', "<", $fecha)
+            ->delete();
+
+        DB::table('password_resets')->where('created_at', '<', $fecha)->delete();
+        
+        if ($deleted) {
+            DestroyEmployeeEvent::dispatch();
         }
     }
 }
