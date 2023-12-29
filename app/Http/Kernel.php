f85a4566351844b707228f0fc6446cc704b75233
Elvis Yerel Roman <yerel9212@yahoo.es>
2023-12-29 13:57:25 -0500

    deny access unverified clients

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,13 +35,15 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class, 
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+            \App\Http\Middleware\VerifyAccount::class,
         ],
 
         'api' => [
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\VerifyAccount::class,
         ],
     ];
 
@@ -69,5 +71,7 @@ class Kernel extends HttpKernel
         'scope' => \App\Http\Middleware\CheckForAnyScope::class,
         'wants.json' => \App\Http\Middleware\ResponseIsJson::class,
         'authorize' => \App\Http\Middleware\DenyGrantType::class,
+        'verify.account' => \App\Http\Middleware\VerifyAccount::class,
+
     ];
 }
