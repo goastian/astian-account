a95d7238aaa330e7ebefceaaf44753cea988a761
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-01-05 22:29:41 -0500

    removed store function for login with API

--- app/Http/Controllers/Auth/AuthorizationController.php
+++ app/Http/Controllers/Auth/AuthorizationController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\Auth\LogoutEvent;
 use App\Http\Controllers\Controller;
-use Laravel\Passport\TokenRepository;
-use App\Http\Requests\Auth\LoginRequest;
-use Elyerr\ApiResponse\Events\LoginEvent;
-use Elyerr\ApiResponse\Events\LogoutEvent;
+use Laravel\Passport\TokenRepository;  
 use Elyerr\ApiResponse\Assets\JsonResponser;
 use Laravel\Passport\RefreshTokenRepository;
 use App\Transformers\Auth\EmployeeTransformer;
@@ -16,31 +14,11 @@ class AuthorizationController extends Controller
     use JsonResponser;
 
     public function __construct()
-    {
-        $this->middleware('guest')->only('store');
+    { 
         $this->middleware('auth:api')->only('destroy');
         $this->middleware('transform.request:' . EmployeeTransformer::class)->only('store');
     }
-    /**
-     * Handle an incoming authentication request.
-     *
-     * @param  \App\Http\Requests\Auth\LoginRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(LoginRequest $request)
-    {
-        $request->authenticate();
-        $scoupes = $request->user()->roles()->get()->pluck('name')->implode(',');
-
-        $token = request()->user()->createToken($_SERVER['HTTP_USER_AGENT'], explode(',', $scoupes))->accessToken;
-
-        LoginEvent::dispatch(request()->user());
-
-        return response()->json(['data' => [
-            'Authorization' => $token,
-        ]], 201);
-    }
-
+     
     /**
      * Destruye la sesion en el cliente invalidado las credenciales.
      *
