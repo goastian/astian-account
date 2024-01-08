f3c455ba4e0a6790a46e0241207be823fb8b3509
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2024-01-08 01:29:39 -0500

    fixed https in production mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,15 @@
 
 namespace App\Providers;
 
-use App\Models\User\Role;
-use App\Models\OAuth\Token;
 use App\Models\OAuth\Client;
+use App\Models\OAuth\PersonalAccessClient;
+use App\Models\OAuth\RefreshToken;
+use App\Models\OAuth\Token;
+use App\Models\User\Role;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\AuthCode;
 use Laravel\Passport\Passport;
-use App\Models\OAuth\RefreshToken;
-use Illuminate\Support\ServiceProvider;
-use App\Models\OAuth\PersonalAccessClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,7 +30,11 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+    {   
+        if (env('SCHEME_MODE') == 'https') {
+            URL::forceScheme(env('SCHEME_MODE'));
+        }
+
         Passport::cookie(env('COOKIE_NAME', 'auth_server'));
 
         Passport::useTokenModel(Token::class);
@@ -37,7 +42,7 @@ class AppServiceProvider extends ServiceProvider
         Passport::useAuthCodeModel(AuthCode::class);
         Passport::useClientModel(Client::class);
         Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
-        
+
         Passport::tokensExpireIn(now()->addSeconds(env('PASSPORT_TOKEN_EXPIRE')));
         Passport::refreshTokensExpireIn(now()->addDays(env('PASSPORT_REFRESH_EXPIRE')));
         Passport::personalAccessTokensExpireIn(now()->addDays(env('PASSPORT_PERSONAL_EXPIRE')));
