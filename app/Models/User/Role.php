6b010b751448286e204d36ac1dc022d621f0be70
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-01-12 17:34:47 -0500

    Added restrictions for channels and roles by default.

--- app/Models/User/Role.php
+++ app/Models/User/Role.php
@@ -3,7 +3,7 @@
 namespace App\Models\User;
 
 use App\Models\Master as master;
-use App\Transformers\Role\RoleTransformer; 
+use App\Transformers\Role\RoleTransformer;
 
 class Role extends master
 {
@@ -18,11 +18,30 @@ class Role extends master
 
     protected $fillable = [
         'name',
-        'description'
+        'description',
     ];
-    
+
     public function users()
     {
-      return  $this->belongsToMany(Employee::class, 'employee_role');
+        return $this->belongsToMany(Employee::class, 'employee_role');
+    }
+
+    public static function default()
+    {
+        return [
+            "admin" => "acceso total al sistema",
+            "broadcast" => "administrar canales",
+            "account" => "administrar cuentas de usuario",
+            "account_read" => "acceso a ver informacion de los usuarios",
+            "account_register" => "registra un nuevo usuario",
+            "account_update" => "actualiza informacion de usaurios",
+            "account_enable" => "permite habilitar usuarios",
+            "account_disable" => "permite deshabilitar usuarios",
+            "scopes" => "administrar scopes",
+            "scopes_read" => "permite ver todos los scopes",
+            "scopes_register" => "permite registrar nuevos scopes",
+            "scopes_update" => "permite actualizar nuevos scopes",
+            "scopes_destroy" => "Permite eliminar un scope",
+        ];
     }
 }
