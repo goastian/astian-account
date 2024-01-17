ffadc65c2e534d8f80815e6f5e449f4f59e1aebc
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-01-17 11:13:28 -0500

    Added support for all devices

--- resources/views/factor/email.blade.php
+++ resources/views/factor/email.blade.php
@@ -1,30 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="mt-5 w-75 text-center" style="margin: auto">
-        <div class="card w-50 bg-dark text-light" style="margin: auto">
+    <div class="mt-5 text-center" style="margin: auto">
+        <div class="factor bg-dark text-light" style="margin: auto">
 
-            <div class="card-head fw-bold text-uppercase mb-5">
+            <div class="factor-head fw-bold text-uppercase mb-5">
                 <h4>
                     {{ __('Two Factor Authentication') }}
                 </h4>
             </div>
-            <div class="body mb-5">
-                <div class="mb-3 text-uppercase">
-                    <label for="token">{{ __('Please type your code 2FA') }}</label>
+            <div class="factor-body mb-5">
+                <div class="mb-3">
+                    <label
+                        for="token">{{ __('We sent a verification code to your email. Enter the code from the email in the field below.') }}</label>
                 </div>
                 <div class="mb-4">
                     <form style="display: inline-block" action="{{ route('factor.email.login') }}" method="post">
-                        <div class="row row-cols-sm-2 col-12 pb-3 border-bottom">
-                            <div class="col">
+                        <div class="row">
+                            <div class="col pt-4">
                                 @csrf
                                 <input type="hidden" id="email" name="email"
                                     value="{{ session('email') ?: old('email') }}">
-                                <input type="text" id="token" name="token" class="form-control d-inline"
+                                <input type="number" id="token" name="token" class="form-control w-75 d-inline"
                                     style="margin: auto">
                             </div>
-                            <div class="col">
-                                <button class="btn d-inline mx-0 mx-0 btn-success">{{ __('Login') }}</button>
+                            <div class="col-12 mt-3 text-sm errors">
+                                @if ($errors->has('token'))
+                                    {{ $errors->first('token') }}
+                                @endif
+                            </div>
+                            <div class="col mt-3">
+                                <button class="btn btn-primary">{{ __('Verify my account') }}</button>
                             </div>
                         </div>
 
@@ -34,11 +40,6 @@
                             @endif
                         </div>
 
-                        <div class="col-12 text-sm errors">
-                            @if ($errors->has('token'))
-                                {{ $errors->first('token') }}
-                            @endif
-                        </div>
                         <div>
                             @foreach ($query as $item => $value)
                                 <input type="hidden" id="{{ $item }}" name="{{ $item }}"
@@ -48,8 +49,8 @@
                     </form>
 
                     <div class="text-justify mt-3">
-                        <a class="btn btn-link btn-block" href="{{ route('login') }}">
-                            {{ __('El codigo ha caducado, por favor intentalo otra vez!') }}
+                        <a class="btn btn-link" href="{{ route('login') }}">
+                            {{ __('The code has expired, please try again!') }}
                         </a>
                     </div>
 
