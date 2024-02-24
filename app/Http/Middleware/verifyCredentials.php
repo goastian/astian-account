dce07c2cd0767e3693ea3239f1479875ff07bc11
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-02-24 12:09:24 -0500

    Ignore the logout route

--- app/Http/Middleware/verifyCredentials.php
+++ app/Http/Middleware/verifyCredentials.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Auth\Session;
 use Closure;
 use Elyerr\ApiResponse\Exceptions\ReportError;
+use ErrorException;
 use Illuminate\Contracts\Encryption\Encrypter as EncrypterContract;
 use Illuminate\Cookie\CookieValuePrefix;
 use Illuminate\Http\Request;
@@ -56,7 +57,7 @@ class verifyCredentials
 
             return $next($request);
         }
-        
+
         throw new ReportError(__('Unauthenticated'), 401);
     }
 
@@ -68,13 +69,19 @@ class verifyCredentials
      */
     public function verifyCookie(Request $request)
     {
-        $user_id = $request->user()->id;
-        $session_name = config('session.cookie');
-        $session_value = $request->cookie($session_name);
-        $decript_session = CookieValuePrefix::remove($this->encrypter->decrypt($session_value, EncryptCookies::serialized($session_name)));
-        $session = Session::find($decript_session);
+        try {
 
-        return $session && $user_id == $session->user_id;
+            $user_id = $request->user()->id;
+            $session_name = config('session.cookie');
+            $session_value = $request->cookie($session_name);
+            $decript_session = CookieValuePrefix::remove($this->encrypter->decrypt($session_value, EncryptCookies::serialized($session_name)));
+            $session = Session::find($decript_session);
+            return $session && $user_id == $session->user_id;
+
+        } catch (ErrorException $e) {
+
+            return strpos($_SERVER['REQUEST_URI'], "gateway/logout") ?: false;
+        }
     }
 
 }
