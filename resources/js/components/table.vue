fd342faeb680b2a5b4d78bf8259f99dac471f3ee
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-06-08 12:15:50 -0500

    updated v-table components

--- resources/js/components/table.vue
+++ resources/js/components/table.vue
@@ -1,22 +1,58 @@
 <template>
-    <div class="table-responsive">
-        <table class="table table-hover">
-            <thead>
-                <th
-                    class="text-uppercase"
-                    v-for="(item, index) in items"
-                    :key="index"
-                    v-text="item"
-                ></th>
-            </thead>
-            <tbody>
-                <slot name="body"></slot>
-            </tbody>
-        </table>
+    <div class="table">
+        <p class="head">
+            <slot name="title"></slot>
+        </p>
+        <div class="table-responsive">
+            <table>
+                <thead>
+                    <slot name="head"></slot>
+                </thead>
+                <tbody>
+                    <slot name="body"></slot>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
-export default {
-    props: ["items"],
-};
+export default {};
 </script>
+
+<style lang="scss" scoped>
+.table {
+    border: 1px solid var(--border-color-light);
+    font-size: 0.8em;
+    color: var(--first-color);
+    border-radius: 0.5em;
+    margin: 1% auto;
+    padding: 0.5em;
+
+    @media (min-width: 800px) {
+        width: 95%;
+    }
+
+    .head {
+        font-size: 1.3em;
+        font-weight: bold;
+        margin-bottom: 0;
+    }
+
+    .table-responsive {
+        overflow: scroll;
+        table {
+            font-size: 0.8em;
+            width: 100%;
+            @media (min-width: 800px) {
+                font-size: 1em;
+            }
+
+            thead {
+            }
+
+            tbody {
+            }
+        }
+    }
+}
+</style>
