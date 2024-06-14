8ac17eda63625eb28e1b6f4368c641a9184f613a
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-06-14 01:14:46 -0500

    Updated token compoents

--- resources/js/pages/OAuth/Personal/Remove.vue
+++ resources/js/pages/OAuth/Personal/Remove.vue
@@ -1,34 +1,13 @@
 <template>
-    <v-modal
-        :target="'__a__'+ token.id"
-        styles="btn-sm btn-danger"
-        button_cancel_name="Cancelar"
-        :button_accept_show="false"
-    >
-        <template v-slot:button> remove </template>
-        <template v-slot:head> Revocar token </template>
-        <template v-slot:body>
-            <div class="text-center text-md">
-                <span class="text-danger"
-                    >Are you sure to destroy this token?</span
-                >
-                <br />
-                <br />
-                <button
-                    @click="removeToken(token)"
-                    class="mx-4 btn btn-block btn-primary"
-                    data-bs-dismiss="modal"
-                >
-                    Aceptar
-                </button>
-            </div>
-        </template>
-    </v-modal>
+    <v-confirm bg="btn-secondary" @is-confirmed="removeToken(token)">
+        <template v-slot:button> Revoke token</template>
+        <template v-slot:head> Revoke token </template>
+        <template v-slot:body> this token will be revoked </template>
+    </v-confirm>
 </template>
-<script> 
-
+<script>
 export default {
-    emits: ["TokenWasRemove"],
+    emits: ["TokenRemoved"],
 
     props: {
         token: {
@@ -38,13 +17,16 @@ export default {
     },
 
     methods: {
-        removeToken(token) {
-            this.$server
-                .delete("/oauth/personal-access-tokens/" + token.id)
-                .then((res) => {
-                    this.$emit("TokenWasRemove", res.data);
-                })
-                .catch((e) => {});
+        async removeToken(token) {
+            try {
+                const res = await this.$server.delete(
+                    "/oauth/personal-access-tokens/" + token.id
+                );
+
+                if (res.status == 204) {
+                    this.$emit("TokenRemoved", res.data);
+                }
+            } catch (e) {}
         },
     },
 };
