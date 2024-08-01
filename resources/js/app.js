46ce35d143a030d10ac9285abec4f7f7822ea3ac
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-07-31 23:14:52 -0500

    Added element plus

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,10 @@ import { components } from "./config/globalComponents";
 import { $channels, $echo } from "./config/echo.js";
 import { $server } from "./config/axios.js";
 
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+ 
 import "./config/matomo.js";
 
 //Loading App
@@ -13,6 +17,9 @@ const app = createApp(App);
 /**
  * Get the user id
  */
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
 
 try {
     const res = await $server.get("/api/gateway/user");
@@ -27,7 +34,7 @@ app.config.globalProperties.$channels = $channels;
 app.config.globalProperties.$server = $server;
 
 app.use(router);
-
+app.use(ElementPlus); 
 /**
  * Global components
  */
