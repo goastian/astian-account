0a2811f56809af651035b71b35c121fbd1aeea68
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-08-03 13:11:37 -0500

    Update resources

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,18 +9,30 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="icon" href="{{ config('app.url') }}/favicon.svg" type="image/svg+xml">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <style>
+        body {
+            display: none;
+        }
+
+        .app-loaded body {
+            display: block;
+        }
+    </style>
+
 </head>
 
 <body>
-    <div class="wrapper">
-
-        <div class="content" id="app">
-        </div>
-
-        @include('layouts.footer')
-
+    <div id="app">
     </div>
     <script src="{{ mix('/js/app.js') }}"></script>
+
+    @include('layouts.footer')
+
+    <script>
+        window.addEventListener('load', function() {
+            document.documentElement.classList.add('app-loaded');
+        });
+    </script>
 </body>
 
 </html>
