ed1a4a6645148a7a521daba831dde822eef37808
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-08-03 13:11:17 -0500

    Fixed the issues with loading vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,37 +9,34 @@ import { $server } from "./config/axios.js";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
- 
-import "./config/matomo.js";
 
-//Loading App
-const app = createApp(App);
-/**
- * Get the user id
- */
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
-}
+import "./config/matomo.js";
 
 try {
     const res = await $server.get("/api/gateway/user");
-
+    /**
+     * Montamos la vue app solo cuando se haya authenticado el usuario
+     */
     if (res.status == 200) {
+
+        const app = createApp(App);
+        for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+            app.component(key, component);
+        }
+
         app.config.globalProperties.$id = res.data.id;
+        app.config.globalProperties.$echo = $echo;
+        app.config.globalProperties.$channels = $channels;
+        app.config.globalProperties.$server = $server;
+        app.use(router);
+        app.use(ElementPlus);
+        /**
+         * Global components
+         */
+        components.forEach((index) => {
+            app.component(index[0], index[1]);
+        });
+
+        app.mount("#app");
     }
 } catch (error) {}
-
-app.config.globalProperties.$echo = $echo;
-app.config.globalProperties.$channels = $channels;
-app.config.globalProperties.$server = $server;
-
-app.use(router);
-app.use(ElementPlus); 
-/**
- * Global components
- */
-components.forEach((index) => {
-    app.component(index[0], index[1]);
-});
-
-app.mount("#app");
