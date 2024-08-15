98cd163f02da59ee004d60034458e9c1c9b4b901
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-08-14 20:54:46 -0500

    Add new controller to manage admin clients

--- app/Models/OAuth/Client.php
+++ app/Models/OAuth/Client.php
@@ -2,10 +2,15 @@
 
 namespace App\Models\OAuth;
 
+use App\Transformers\OAuth\ClientAdminTransformer;
 use Elyerr\ApiResponse\Assets\Timestamps;
 use Laravel\Passport\Client as PassportClient;
 
 class Client extends PassportClient
 {
     use Timestamps;
+
+    public $table = "oauth_clients";
+
+    public $transformer = ClientAdminTransformer::class;
 }
