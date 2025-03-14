5858842390d0424be6824f749a3db3d4f8de8eef
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-03-14 08:56:51 -0500

    feat: added navigation design for the user

--- resources/js/app/pages/AuthLayout.vue
+++ resources/js/app/pages/AuthLayout.vue
@@ -1,62 +1,79 @@
 <template>
-    <q-layout view="hHh lpR lFf" v-if="$user.id">
-        <q-header elevated class="bg-primary text-white">
+    <q-layout view="hHh Lpr lff" v-if="$user.id">
+        <q-header elevated class="">
             <q-toolbar>
                 <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
                 <q-toolbar-title> {{ $app_name }} </q-toolbar-title>
                 <q-space />
-                <v-profile></v-profile>
+                <v-profile />
             </q-toolbar>
         </q-header>
 
         <q-drawer
+            v-mode="leftDrawerOpen"
             show-if-above
-            v-model="leftDrawerOpen"
-            side="left"
-            behavior="desktop"
-            bordered
+            :width="350"
+            :breakpoint="400"
         >
-            <q-list bordered>
-                <q-expansion-item
-                    v-for="(menu, index) in menus"
-                    :key="index"
-                    expand-separator
-                    :label="menu.name"
-                    :icon="menu.icon"
-                    v-show="menu.show"
-                >
+            <q-scroll-area class="nav-sroll">
+                <q-list class="nav-list column q-gutter-y-sm">
+                    <q-expansion-item
+                        v-if="$user.groups[0].slug == 'administrator'"
+                        v-for="(menu, index) in menus"
+                        :key="index"
+                        expand-separator
+                        :label="menu.name"
+                        :icon="menu.icon"
+                        v-show="menu.show"
+                    >
+                        <q-item
+                            v-for="(item, i) in menu.menu"
+                            :key="i"
+                            clickable
+                            v-ripple
+                            @click="open(item)"
+                        >
+                            <q-item-section avatar>
+                                <q-icon color="blue" :name="item.icon" />
+                            </q-item-section>
+                            <q-item-section>
+                                {{ item.name }}
+                            </q-item-section>
+                        </q-item>
+                    </q-expansion-item>
                     <q-item
-                        v-for="(item, i) in menu.menu"
-                        :key="i"
+                        v-else
+                        v-for="(item, index) in nav"
+                        :key="index"
                         clickable
-                        v-ripple
                         @click="open(item)"
+                        class="item-user"
+                        :active="$route.name == item.route"
+                        active-class="active-link"
                     >
                         <q-item-section avatar>
-                            <q-icon color="blue" :name="item.icon" />
+                            <q-icon :name="item.icon" />
                         </q-item-section>
                         <q-item-section>
                             {{ item.name }}
                         </q-item-section>
                     </q-item>
-                </q-expansion-item>
-            </q-list>
+                </q-list>
+            </q-scroll-area>
         </q-drawer>
 
         <q-page-container>
-            <div class="px-2 py-2">
-                <router-view />
-            </div>
+            <router-view />
         </q-page-container>
-    </q-layout>
 
-    <q-page v-if="!$user.id" class="fixed-full flex flex-center bg-white">
-        <div class="text-center">
-            <q-spinner size="3rem" color="indigo" class="q-mb-md" />
-            <p class="text-h6 text-grey-7 q-animate--pulse">Loading ...</p>
-        </div>
-    </q-page>
+        <q-page v-if="!$user.id" class="fixed-full flex flex-center bg-white">
+            <div class="text-center">
+                <q-spinner size="3rem" color="indigo" class="q-mb-md" />
+                <p class="text-h6 text-grey-7 q-animate--pulse">Loading ...</p>
+            </div>
+        </q-page>
+
+    </q-layout>
 </template>
 
 <script>
@@ -68,6 +85,33 @@ export default {
             drawer: true,
             errors: {},
             leftDrawerOpen: false,
+            nav: [
+                {
+                    name: "Dashboard",
+                    icon: "mdi-home",
+                    route: "about",
+                },
+                {
+                    name: "Personal Information",
+                    icon: "mdi-account",
+                    route: "information"
+                },
+                {
+                    name: "Security",
+                    icon: "mdi-shield-outline",
+                    route: "security"
+                },
+                {
+                    name: "Payment and Suscriptions",
+                    icon: "mdi-account-credit-card",
+                    route: "payment"
+                },
+                {
+                    name: "Help",
+                    icon: "mdi-help",
+                    route: "help"
+                }
+            ],
             menus: [
                 {
                     name: "Account",
@@ -175,5 +219,48 @@ export default {
 <style scoped lang="css">
 .custom-drawer {
     width: 250px;
+}
+
+.nav-top {
+    height: 60px;
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+}
+
+.nav-top .q-img {
+    width: 30px;
+    height: 30px;
+}
+
+.nav-top h1 {
+    font-size: 1rem;
+}
+
+.nav-sroll {
+    height: 100%;
+    padding: 1rem 0;
+}
+
+.nav-list {
+    padding: 0 1rem;
+}
+
+.item-user {
+    border-radius: 2rem;
+    padding: 0 2.5rem;
+    color: var(--text-link);
+}
+
+.active-link {
+    background-color: var(--bg-link-active);
+    color: var(--text-link-active);
+}
+
+.item-user:hover {
+    background-color: var(--bg-link-hover);
+    color: var(--text-link-hover);
 }
 </style>
