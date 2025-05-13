fa7678917ccf4765cb488f36454d1e758d928eb5
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-05-12 21:01:56 -0500

    chore: updated vue components

--- resources/js/app/components/VProfile.vue
+++ resources/js/app/components/VProfile.vue
@@ -1,50 +1,69 @@
 <template>
     <div class="text-center">
-        <q-btn flat round icon="mdi-dots-vertical">
+        <q-btn flat round dense icon="mdi-dots-vertical">
             <q-menu fit anchor="bottom right" self="top right">
-                <q-card style="min-width: 200px">
-                    <q-list>
-                        <q-item v-if="user?.id">
+                <q-card style="min-width: 240px" class="q-pa-sm">
+                    <!-- User Info -->
+                    <div v-if="user?.id" class="q-pa-sm flex items-center">
+                        <q-avatar size="40px" class="q-mr-sm">
+                            <q-avatar
+                                size="40px"
+                                class="q-mr-sm bg-primary text-white"
+                            >
+                                <q-icon name="mdi-account-circle" size="28px" />
+                            </q-avatar>
+                        </q-avatar>
+                        <div>
+                            <div class="text-weight-medium">
+                                {{ user.name }} {{ user.last_name }}
+                            </div>
+                            <div class="text-caption text-grey">
+                                {{ user.email }}
+                            </div>
+                        </div>
+                    </div>
+
+                    <q-separator class="q-my-sm" />
+
+                    <!-- Menu Options -->
+                    <q-list padding>
+                        <q-item v-if="!user?.id" clickable @click="homePage">
+                            <q-item-section avatar>
+                                <q-icon name="mdi-home" />
+                            </q-item-section>
                             <q-item-section>
-                                <q-item-label>
-                                    {{ user.name }}
-                                    {{ user.last_name }}
-                                </q-item-label>
-                                <q-item-label caption>
-                                    {{ user.email }}
-                                </q-item-label>
+                                <q-item-label>Home Page</q-item-label>
                             </q-item-section>
                         </q-item>
-                    </q-list>
 
-                    <q-separator />
-
-                    <q-list>
-                        <q-item
-                            v-if="!user?.id"
-                            icon="mdi-home"
-                            clickable
-                            @click="homePage"
-                        >
-                            <q-item-section> Home page </q-item-section>
-                        </q-item>
                         <q-item
                             v-if="user?.id"
-                            icon="mdi-home-account"
                             clickable
                             @click="goToDashboard"
                         >
-                            <q-item-section> My account </q-item-section>
+                            <q-item-section avatar>
+                                <q-icon name="mdi-home-account" />
+                            </q-item-section>
+                            <q-item-section>
+                                <q-item-label>My Account</q-item-label>
+                            </q-item-section>
                         </q-item>
-                        <q-separator />
+
+                        <q-separator class="q-my-sm" v-if="user?.id" />
+
                         <q-item
                             v-if="user?.id"
                             clickable
                             v-close-popup
                             @click="logout"
                         >
+                            <q-item-section avatar>
+                                <q-icon name="mdi-logout" />
+                            </q-item-section>
                             <q-item-section>
-                                <q-item-label>Logout</q-item-label>
+                                <q-item-label class="text-negative"
+                                    >Logout</q-item-label
+                                >
                             </q-item-section>
                         </q-item>
                     </q-list>
@@ -65,9 +84,12 @@ export default {
     methods: {
         async logout() {
             try {
-                const res = await this.$server.post(this.$page.props.logout);
+                const res = await this.$server.post(
+                    this.$page.props.auth_routes["logout"]
+                );
                 if (res.status === 200) {
-                    window.location.href = this.$page.props.login;
+                    window.location.href =
+                        this.$page.props.auth_routes["login"];
                 }
             } catch (error) {
                 console.error("Error logging out:", error);
