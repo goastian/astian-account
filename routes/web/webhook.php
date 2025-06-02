26cf6a014bb943393a7c90cb2b07bd5d7d20479f
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-06-02 16:29:28 -0500

    chore: refactoring payment methods

--- routes/web/webhook.php
+++ routes/web/webhook.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Models\Payment\Webhook\StripeWebhookController;
+use Illuminate\Support\Facades\Route; 
+use App\Services\Payment\Webhook\StripeWebhookController;
 
 
 Route::group([
