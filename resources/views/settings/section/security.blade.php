12f66baac48eca93219c7b5a12a562f2bd0dfdc7
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-06-03 13:53:27 -0500

    fix: fixed script in security section

--- resources/views/settings/section/security.blade.php
+++ resources/views/settings/section/security.blade.php
@@ -29,7 +29,7 @@
                         {{ __('Captcha driver') }}
                     </label>
 
-                    <select name="services[captcha][driver]"
+                    <select id="captcha" name="services[captcha][driver]"
                         class="block w-full px-2 py-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                         <option value="" disabled>{{ __('Choose option') }}</option>
                         <option value="turnstile" {{ config('services.captcha.driver') == 'turnstile' ? 'selected' : '' }}>
@@ -39,75 +39,109 @@
                             {{ __('hcaptcha') }}
                         </option>
                     </select>
+
+
+                    <div class="mb-2">
+                        <label class="block text-sm font-medium text-gray-700 mb-2">
+                            {{ __('Enable CAPTCHA') }}
+                        </label>
+                        <select name="services[captcha][enabled]"
+                            class="block w-full px-2 py-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                            <option value="" disabled>{{ __('Choose option') }}</option>
+                            <option value="{{ true }}"
+                                {{ config('services.captcha.enabled') == true ? 'selected' : '' }}>
+                                {{ __('Yes') }}
+                            </option>
+                            <option value="{{ false }}"
+                                {{ config('services.captcha.enabled') == false ? 'selected' : '' }}>
+                                {{ __('No') }}
+
+                            </option>
+                        </select>
+                    </div>
                 </div>
 
-                <div class="mb-2">
-                    <label class="block text-sm font-medium text-gray-700 mb-2">
-                        {{ __('Enable CAPTCHA') }}
-                    </label>
-                    <select name="services[captcha][enabled]"
-                        class="block w-full px-2 py-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                        <option value="" disabled>{{ __('Choose option') }}</option>
-                        <option value="{{ true }}"
-                            {{ config('services.captcha.enabled') == true ? 'selected' : '' }}>
-                            {{ __('Yes') }}
-                        </option>
-                        <option value="{{ false }}"
-                            {{ config('services.captcha.enabled') == false ? 'selected' : '' }}>
-                            {{ __('No') }}
+                <div id="turnstile" class="border-gray-300 hidden rounded-lg p-4 bg-white shadow-sm">
+                    <h3 class="text-lg font-semibold text-gray-700 mb-2 border-b pb-2">{{ __('Cloudflare Turnstile') }}
+                    </h3>
+                    <div class="mb-2">
+                        <label class="block text-sm font-medium text-gray-700">Endpoint</label>
+                        <input type="text" name="services[captcha][providers][turnstile][api]"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
+                            value="{{ config('services.captcha.providers.turnstile.api') }}">
+                    </div>
 
-                        </option>
-                    </select>
-                </div>
-            </div>
+                    <div class="mb-2">
+                        <label class="block text-sm font-medium text-gray-700">Site key</label>
+                        <input type="text" name="services[captcha][providers][turnstile][sitekey]"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
+                            value="{{ config('services.captcha.providers.turnstile.sitekey') }}">
+                    </div>
 
-            <div class="border-gray-300 rounded-lg p-4 bg-white shadow-sm">
-                <h3 class="text-lg font-semibold text-gray-700 mb-2 border-b pb-2">{{ __('Cloudflare Turnstile') }}</h3>
-                <div class="mb-2">
-                    <label class="block text-sm font-medium text-gray-700">Endpoint</label>
-                    <input type="text" name="services[captcha][providers][turnstile][api]"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
-                        value="{{ config('services.captcha.providers.turnstile.api') }}">
+                    <div class="mb-2">
+                        <label class="block text-sm font-medium text-gray-700">Secret key</label>
+                        <input type="password" name="services[captcha][providers][turnstile][secret]"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
+                            value="{{ config('services.captcha.providers.turnstile.secret') }}">
+                    </div>
                 </div>
 
-                <div class="mb-2">
-                    <label class="block text-sm font-medium text-gray-700">Site key</label>
-                    <input type="text" name="services[captcha][providers][turnstile][sitekey]"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
-                        value="{{ config('services.captcha.providers.turnstile.sitekey') }}">
-                </div>
+                <div id="hcaptcha" class="border-gray-300 hidden rounded-lg p-4 bg-white shadow-sm">
+                    <h3 class="text-lg font-semibold text-gray-700 mb-2 border-b pb-2">{{ __('Hcaptcha') }}</h3>
+                    <div class="mb-2">
+                        <label class="block text-sm font-medium text-gray-700">Endpoint</label>
+                        <input type="text" name="services[captcha][providers][hcaptcha][api]"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
+                            value="{{ config('services.captcha.providers.hcaptcha.api') }}">
+                    </div>
 
-                <div class="mb-2">
-                    <label class="block text-sm font-medium text-gray-700">Secret key</label>
-                    <input type="password" name="services[captcha][providers][turnstile][secret]"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
-                        value="{{ config('services.captcha.providers.turnstile.secret') }}">
-                </div>
-            </div>
+                    <div class="mb-2">
+                        <label class="block text-sm font-medium text-gray-700">Site key</label>
+                        <input type="text" name="services[captcha][providers][hcaptcha][sitekey]"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
+                            value="{{ config('services.captcha.providers.hcaptcha.sitekey') }}">
+                    </div>
 
-            <div class="border-gray-300 rounded-lg p-4 bg-white shadow-sm">
-                <h3 class="text-lg font-semibold text-gray-700 mb-2 border-b pb-2">{{ __('Hcaptcha') }}</h3>
-                <div class="mb-2">
-                    <label class="block text-sm font-medium text-gray-700">Endpoint</label>
-                    <input type="text" name="services[captcha][providers][hcaptcha][api]"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
-                        value="{{ config('services.captcha.providers.hcaptcha.api') }}">
-                </div>
-
-                <div class="mb-2">
-                    <label class="block text-sm font-medium text-gray-700">Site key</label>
-                    <input type="text" name="services[captcha][providers][hcaptcha][sitekey]"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
-                        value="{{ config('services.captcha.providers.hcaptcha.sitekey') }}">
-                </div>
-
-                <div class="mb-2">
-                    <label class="block text-sm font-medium text-gray-700">Secret key</label>
-                    <input type="password" name="services[captcha][providers][hcaptcha][secret]"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
-                        value="{{ config('services.captcha.providers.hcaptcha.secret') }}">
+                    <div class="mb-2">
+                        <label class="block text-sm font-medium text-gray-700">Secret key</label>
+                        <input type="password" name="services[captcha][providers][hcaptcha][secret]"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-300"
+                            value="{{ config('services.captcha.providers.hcaptcha.secret') }}">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
+@push('js')
+    <script nonce="{{ $nonce }}">
+        window.document.addEventListener("DOMContentLoaded", function() {
+
+            const captcha = document.getElementById("captcha")
+            const turnstile = document.getElementById("turnstile");
+            const hcaptcha = document.getElementById("hcaptcha");
+
+            show(captcha.value)
+
+            captcha.addEventListener("change", function(dom) {
+                show(dom.target.value)
+            })
+
+            function show(value) {
+                switch (value) {
+                    case 'turnstile':
+                        turnstile.classList.remove('hidden')
+                        hcaptcha.classList.add('hidden')
+                        break;
+                    case "hcaptcha":
+                        turnstile.classList.add('hidden')
+                        hcaptcha.classList.remove('hidden')
+                        break;
+                    default:
+                        break;
+                }
+            }
+        })
+    </script>
+@endpush
