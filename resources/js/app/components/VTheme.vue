6dd97f23b9f053bbefffa8b22be95758614e22db
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-05 18:15:09 -0500

    feat: improve design for light and dark mode

--- resources/js/app/components/VTheme.vue
+++ resources/js/app/components/VTheme.vue
@@ -1,108 +1,117 @@
 <template>
-    <div>
-        <q-select
-            filled
-            dense
-            v-model="selectedTheme"
-            :options="themes"
-            label="Choose theme"
-            option-value="value"
-            option-label="label"
-            emit-value
-            map-options
-            @update:model-value="applyTheme"
-            :option-slot="true"
-            popup-content-class="theme-selector-popup"
-        >
-            <template v-slot:option="scope">
-                <q-item v-bind="scope.itemProps">
-                    <q-item-section avatar>
-                        <q-icon
-                            :name="scope.opt.icon"
-                            :color="scope.opt.color"
-                        />
-                    </q-item-section>
-                    <q-item-section>
-                        <q-item-label>{{ scope.opt.label }}</q-item-label>
-                    </q-item-section>
-                </q-item>
+    <div class="container" ref="dropdown">
+        <span class="label">Theme</span>
+        <div class="row select" @click="chageState">
+            <span><q-icon :name="theme.selectedTheme.icon" /></span>
+            <span>
+                {{ theme.selectedTheme.label }}
+            </span>
+        </div>
+        <div class="column list" v-if="state">
+            <template
+                v-for="(item, index) in theme.themes"
+            >
+                <div
+                    class="item row"
+                    :class="{'q-item--active' : item.value == theme.selectedTheme.value}"
+                    @click="changeTheme(item)"
+                >
+                    <span><q-icon :name="item.icon" /></span>
+                    <span>{{ item.label }}</span>
+                </div>
             </template>
-
-            <template v-slot:selected>
-                <q-item-section avatar>
-                    <q-icon :name="selectedIcon" :color="selectedColor" />
-                </q-item-section>
-                <q-item-section>
-                    <q-item-label>{{ selectedLabel }}</q-item-label>
-                </q-item-section>
-            </template>
-        </q-select>
+        </div>
     </div>
 </template>
 
 <script>
+import useThemeStore from '../stores/useThemeStore.js';
 export default {
     data() {
         return {
-            selectedTheme: "",
-            themes: [
-                {
-                    label: "Light (Light)",
-                    value: "light",
-                    icon: "light_mode",
-                    color: "primary",
-                },
-                {
-                    label: "Dark (Dark)",
-                    value: "dark",
-                    icon: "dark_mode",
-                    color: "grey-8",
-                },
-                {
-                    label: "Red (Red)",
-                    value: "red",
-                    icon: "palette",
-                    color: "red",
-                },
-            ],
+            theme: useThemeStore(),
+            state: false,
         };
     },
-    computed: {
-        selectedLabel() {
-            return (
-                this.themes.find((t) => t.value === this.selectedTheme)
-                    ?.label || "Choose theme"
-            );
-        },
-        selectedIcon() {
-            return (
-                this.themes.find((t) => t.value === this.selectedTheme)?.icon ||
-                "palette"
-            );
-        },
-        selectedColor() {
-            return (
-                this.themes.find((t) => t.value === this.selectedTheme)
-                    ?.color || "primary"
-            );
-        },
+
+    created() {
+        const $q = this.$q;
+        this.theme.loadTheme($q)
     },
+
     mounted() {
-        const theme = localStorage.getItem("theme") || "light";
-        this.selectedTheme = theme;
-        this.applyTheme(theme);
+        document.addEventListener('click', this.handleClickOutside);
     },
+
+    beforeUnmount() {
+        document.removeEventListener('click', this.handleClickOutside)
+    },
+
     methods: {
-        applyTheme(theme) {
-            document.body.setAttribute("data-theme", theme);
-            localStorage.setItem("theme", theme);
-            this.$q.dark.set(theme === "dark");
+        chageState() {
+            this.state = !this.state;
         },
-    },
+
+        handleClickOutside(event) {
+            if (this.$refs.dropdown && !this.$refs.dropdown.contains(event.target)) {
+                this.state = false
+            }
+        },
+
+        changeTheme(item) {
+            this.theme.changeTheme(item, this.$q);
+        },
+    }
 };
 </script>
 
 <style scoped>
+.container {
+    width: 200px;
+    position: relative;
+}
+
+.label {
+    position: absolute;
+    top: -10px;
+    left: 10px;
+    font-size: .7rem;
+}
+
+.select {
+    width: 200px;
+    background-color: var(--q-background-primary);
+    border: 1px solid var(--q-border);
+    padding: .4rem .4rem;
+    border-radius: .6rem;
+    gap: 1rem;
+    cursor: pointer;
+}
+
+.list {
+    position: absolute;
+    width: 100%;
+    bottom: -120px;
+    left: 0;
+    background-color: var(--q-background-primary);
+    padding: .2rem;
+    border-radius: .6rem;
+    border: 1px solid var(--q-border);
+    gap: .2rem;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.06);
+}
+
+.item {
+    gap: 1rem;
+    padding: .4rem;
+    border-radius: .5rem;
+    cursor: pointer;
+}
+
+.item:hover {
+    background-color: var(--q-color-light);
+}
+
 .theme-selector-popup {
     max-height: 200px;
 }
