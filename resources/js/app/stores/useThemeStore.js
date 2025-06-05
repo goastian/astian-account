ccc6a7f191080b6808f106f0b6109895f3c35748
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-05 18:09:01 -0500

    feat: create theme store with dark mode state

---
+++ resources/js/app/stores/useThemeStore.js
@@ -0,0 +1,48 @@
+import { defineStore } from 'pinia';
+
+const useThemeStore = defineStore('theme', {
+    state: () => ({
+        selectedTheme: {
+            label: 'Light',
+            value: 'light',
+            icon: 'light_mode',
+            color: 'primary',
+        },
+        themes: [
+            {
+                label: "Light",
+                value: "light",
+                icon: "light_mode",
+                color: "primary",
+            },
+            {
+                label: "Dark",
+                value: "dark",
+                icon: "dark_mode",
+                color: "grey-8",
+            },
+            {
+                label: "Red",
+                value: "red",
+                icon: "palette",
+                color: "red",
+            },
+        ],
+    }),
+
+    actions: {
+        loadTheme($q) {
+            document.body.setAttribute("data-theme", this.selectedTheme.value);
+            localStorage.setItem("theme", this.selectedTheme.value);
+            $q.dark.set(this.selectedTheme.value === "dark");
+        },
+
+        changeTheme(select, $q) {
+            this.selectedTheme = select;
+            this.loadTheme($q);
+        }
+    },
+    persist: true
+});
+
+export default useThemeStore;
