a0940184210f8e52e4dc03d7196b2af34ab12dcf
Im-Jota <joseluisromeroarce12@gmail.com>
2025-06-13 16:51:04 -0500

    feat: improve payment design

--- resources/js/app/pages/Resources/Pay.vue
+++ resources/js/app/pages/Resources/Pay.vue
@@ -6,10 +6,7 @@
             <div class="row container-plan justify-center">
                 <div class="card left col column">
                     <div class="row items-center top">
-                        <span
-                            class="icon logo"
-                            v-if="plan.scopes[0].service.name == 'vpn'"
-                        >🛡️</span>
+                        <span class="icon logo" v-if="plan.scopes[0].service.name == 'vpn'">🛡️</span>
                         <div class="column">
                             <span>{{ plan.scopes[0].role.name }}</span>
                             <span class="title strong">{{ plan.name }}</span>
@@ -18,34 +15,28 @@
                     <div class="container-price row justify-center items-center">
                         <template v-for="item in plan.prices">
                             <div v-if="item.billing_period == params.billing_period">
-                                <span
-                                    class="price"
-                                    v-if="item.currency == 'USD'"
-                                >$</span>
-                                <span
-                                    class="price"
-                                    v-else
-                                >{{ item.currency }}</span>
+                                <span class="price" v-if="item.currency == 'USD'">$</span>
+                                <span class="price" v-else>{{ item.currency }}</span>
                                 <span class="price">{{ item.amount_format }}</span>
                                 <span>/{{ item.billing_period }}</span>
                             </div>
                         </template>
                     </div>
                     <div class="column description">
-                        <h6 class="subTitle strong">Todo lo que incluye:</h6>
+                        <h6 class="subTitle strong">What's included:</h6>
                         <div v-html="plan.description"></div>
                     </div>
                     <div class="tip">
-                        <span>💡 <strong>Tips:</strong> Puedes cancelar en cualquier momento desde tu panel de control.</span>
+                        <span>💡 <strong>Tips:</strong> You can cancel at any time from your dashboard.</span>
                     </div>
                 </div>
 
                 <div class="col column right">
-                    <div class="card">
-                        <div v-if="user " class="column gap-1">
+                    <div class="card column gap-1">
+                        <div v-if="user" class="column gap-1">
                             <div class="subTitle strong row items-center">
                                 <span><q-icon name="mdi-account" /></span>
-                                <span>Información del usuario</span>
+                                <span>User Information</span>
                             </div>
                             <div class="row items-center gap-05 user-information">
                                 <span class="icon user row justify-center items-center"><q-icon name="check" /></span>
@@ -54,31 +45,38 @@
                                     <span class="sub">{{ user.email }}</span>
                                 </div>
                             </div>
+                            <!--
                             <div>
                                 <button>Cambiar de cuenta</button>
                             </div>
+                            -->
                         </div>
 
                         <div v-else class="column gap-1">
                             <div>
                                 <div class="subTitle strong row items-center">
                                     <span><q-icon name="mdi-alert" /></span>
-                                    <span>Acción requerida</span>
+                                    <span>Action required</span>
                                 </div>
                                 <div>
-                                    <span>Necesitas iniciar sesión para continuar con el pago y activar tu suscripción.</span>
+                                    <span>You need to log in to continue with the payment and activate your
+                                        subscription.</span>
                                 </div>
                             </div>
                             <div class="column items-start gap-05 card-warning">
-                                <span>¿Por qué necesito una cuenta?</span>
+                                <span>Why do I need an account?</span>
                                 <ul>
-                                    <li class="">🔹 Gestionar tu suscripción</li>
-                                    <li class="">🔹 Acceder a tu dashboard</li>
+                                    <li class="">🔹 Manage your subscription</li>
+                                    <li class="">🔹 Access your dashboard</li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
 
+                        <div>
+                            <button class="btn suscribe" @click="payment" :disabled="!user">Suscribe Now</button>
+                        </div>
+                    </div>
+                    <!--
                     <div class="card column gap-1">
                         <div class="subTitle strong row items-center">
                             <span><q-icon name="mdi-credit-card-outline" /></span>
@@ -101,18 +99,20 @@
                             </div>
                         </div>
                         <div>
-                            <button class="btn suscribe">Suscribe Now</button>
+                            <button class="btn suscribe" @click="payment">Suscribe Now</button>
                         </div>
                     </div>
+                    -->
                 </div>
             </div>
         </template>
     </v-guest-layout>
 
-    <div
-        v-else
-    >
-        <span>loading</span>
+    <div v-else>
+        <div class="loading-container" role="status" aria-live="polite">
+            <div class="spinner" aria-hidden="true"></div>
+            <div class="loading-text">Loading, please wait...</div>
+        </div>
     </div>
 </template>
 
@@ -124,20 +124,34 @@ export default {
             plan: null,
             loading: true,
             params: {},
+            methods: [],
         }
     },
 
     mounted() {
         this.getPlan();
+        this.getBillingPeriod();
     },
 
     created() {
-        if(!Array.isArray(this.$page.props.user)) {
+        if (!Array.isArray(this.$page.props.user)) {
             this.user = this.$page.props.user;
         }
     },
 
     methods: {
+        async getBillingPeriod() {
+            try {
+                const res = await this.$server.get(
+                    "/api/public/payments/methods"
+                );
+
+                if (res.status == 200) {
+                    this.methods = res.data.data;
+                }
+            } catch (error) { }
+        },
+
         async getPlan() {
             const urlParams = new URLSearchParams(window.location.search);
 
@@ -153,9 +167,11 @@ export default {
                     params: this.params,
                 });
 
-                console.log(res.data.data)
-
                 if (res.status === 200) {
+                    if (res.data.data.length == 0) {
+                        const url = this.$page.props.guest_routes['plans'];
+                        window.location.href = url;
+                    }
                     this.plan = res.data.data[0];
                     this.loading = false;
                 }
@@ -164,6 +180,51 @@ export default {
                     message: "Error cargando el plan.",
                     type: "negative",
                 });
+                const url = this.$page.props.guest_routes['plans'];
+                window.location.href = url;
+            }
+        },
+
+        async payment() {
+            if (!this.user?.id) {
+                this.guest = true;
+                this.$q.notify({
+                    type: "negative",
+                    message: "Please login and try again",
+                    timeout: 3000,
+                });
+                return;
+            }
+
+            if (!this.user?.id) {
+                this.$q.notify({
+                    type: "negative",
+                    message: "Please select the plan to continue ...",
+                    timeout: 3000,
+                });
+                return;
+            }
+
+            await this.continuePayment();
+        },
+
+        async continuePayment() {
+            this.disabled = true;
+            try {
+                const res = await this.$server.post(
+                    this.user.links.subscriptions_buy,
+                    {
+                        plan: this.plan.id,
+                        billing_period: this.params.billing_period,
+                        payment_method: 'stripe',
+                    }
+                );
+
+                if (res.status == 201) {
+                    window.location.href = res.data.data.redirect_to;
+                }
+            } catch (error) {
+                this.disabled = false;
             }
         },
     }
@@ -292,7 +353,7 @@ export default {
     border: 1px solid var(--q-color-blue);
 }
 
-.card-method > .icon {
+.card-method>.icon {
     width: 50px;
     height: 50px;
     background-color: var(--q-background-secondary);
@@ -300,7 +361,7 @@ export default {
     font-size: 1.5rem;
 }
 
-.card-method.active > .icon {
+.card-method.active>.icon {
     background-color: var(--q-primary);
     color: white;
 }
@@ -311,5 +372,48 @@ export default {
     border-radius: .5rem;
     background-color: var(--q-primary);
     color: white;
+}
+
+.loading-container {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--q-background-primary);
+}
+
+/* El círculo animado */
+.spinner {
+    width: 70px;
+    height: 70px;
+    border: 8px solid #ddd;
+    border-top-color: #4a90e2;
+    /* color principal */
+    border-radius: 50%;
+    animation: spin 1.3s linear infinite;
+    box-shadow: 0 0 10px rgba(74, 144, 226, 0.5);
+}
+
+/* Animación de giro */
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+/* Mensaje amigable */
+.loading-text {
+    margin-top: 20px;
+    font-size: 1.2rem;
+    color: #555;
+    font-weight: 600;
+    user-select: none;
+}
+
+@media (max-width: 700px) {
+    .container-plan {
+        flex-direction: column;
+    }
 }
 </style>
