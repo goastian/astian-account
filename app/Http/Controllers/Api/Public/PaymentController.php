12d2477dca4ba30bc1709c99d526c43d586790b2
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-06-22 01:28:19 -0500

    fix(api): endpoints

--- app/Http/Controllers/Api/Public/PaymentController.php
+++ app/Http/Controllers/Api/Public/PaymentController.php
@@ -1,10 +1,24 @@
 <?php
 namespace App\Http\Controllers\Api\Public;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\ServiceRepository;
 
 class PaymentController extends Controller
 {
+
+    /**
+     * Repository
+     * @var 
+     */
+    public $repository;
+
+    public function __construct(ServiceRepository $serviceRepository)
+    {
+        $this->repository = $serviceRepository;
+    }
+
     /**
      * Get the billing period
      * @return mixed|\Illuminate\Http\JsonResponse
@@ -12,15 +26,6 @@ class PaymentController extends Controller
     public function billingPeriod()
     {
         return $this->data(['data' => billing_periods()]);
-    }
-
-    /**
-     * show the currencies
-     * @return mixed|\Illuminate\Http\JsonResponse
-     */
-    public function currencies()
-    {
-        return $this->data(['data' => billing_currencies()]);
     }
 
     /**
@@ -39,5 +44,15 @@ class PaymentController extends Controller
     public function methods()
     {
         return $this->data(['data' => billing_methods()]);
+    }
+
+    /**
+     * show the all services
+     * @param \Illuminate\Http\Request $request
+     * @return \Elyerr\ApiResponse\Assets\JsonResponser
+     */
+    public function services(Request $request)
+    {
+        return $this->repository->searchForGuest($request);
     }
 }
