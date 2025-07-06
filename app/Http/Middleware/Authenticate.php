a0da88432f6d3c0d4aadf6b905021a429e8208d9
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-07-06 12:04:36 -0500

    fix: apply strict validation for oauth2 credentials

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,11 +2,44 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Elyerr\ApiResponse\Exceptions\ReportError;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+        $this->authenticate($request, $guards);
+
+        // Checking authentication with oauth2
+        if ($request->bearerToken()) {
+
+            $token = $request->user()->token();
+
+            // Checking Authentication
+            if (!$request->user() || !$token || empty($token->client) || $token->revoked) {
+                throw new AuthenticationException;
+            }
+        }
+
+        return $next($request);
+    }
+
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
