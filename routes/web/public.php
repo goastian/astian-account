70a48a2dfcfac34800e082d9ef11e11a832f7fb6
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-07-27 19:05:54 -0500

    fix: deleted home page
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- routes/web/public.php
+++ routes/web/public.php
@@ -1,9 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Web\Home\HomeController;
+use Illuminate\Support\Facades\Route; 
 use App\Http\Controllers\Web\Home\PlanController;
-
-Route::get("/", [HomeController::class, 'homePage'])->name('home.page');
 
 Route::get('/plans', [PlanController::class, 'index'])->name('plans.index');
