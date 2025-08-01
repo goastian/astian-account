ff40b812a741cf1fcf3b16639fa075d0357c924f
Walddys Emmanuel Dorrejo Céspedes <dev.dorrejo@hotmail.com>
2025-08-01 18:47:04 -0400

    Actualización de permisos por tipo de archivos

--- docker/laravel-scripts.sh
+++ docker/laravel-scripts.sh
@@ -6,8 +6,16 @@ cd /var/www
 echo "⚙️ Running system configuration..."
 php artisan settings:system-start
 
-chown -R www-data:www-data /var/www
-chmod -R 775 /var/www
+# chown -R www-data:www-data /var/www
+# chmod -R 775 /var/www
+
+# File Permissions
+find /var/www -type f -exec chmod 664 '{}' \+
+
+# Directory Permissions
+find /var/www -type d -exec chmod 775 '{}' \+
+
+chmod -R 444 /var/www/.env
 chmod 600 secrets/oauth/*.key
 
 echo "🚀 Starting PHP-FPM..."
